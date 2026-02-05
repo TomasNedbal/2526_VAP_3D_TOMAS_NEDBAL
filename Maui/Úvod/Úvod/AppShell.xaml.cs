6c62d3ace425769ff777affc04aa263c5e75b973
3.D Nedbal Tomáš <Tomas.Nedbal@student.sps-cl.cz>
2026-02-05 16:11:05 +0100

    a

---
+++ Maui/Úvod/Úvod/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Úvod
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Úvod/Úvod/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿//hodnotové datové typy
-int cislo = 0;
-int cislo2 = cislo;
-cislo = 1;
-
-Console.WriteLine(cislo + ", " + cislo2);
