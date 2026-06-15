5feb4aa760a8fd7678e35aa62b75404a1d613836
3.D Nedbal Tomáš <Tomas.Nedbal@student.sps-cl.cz>
2026-06-15 10:53:32 +0200

    commit

---
+++ NET MAUI/Úvod/Úvod/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Úvod
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NET MAUI/Úvod/Úvod/MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Úvod
+{
+    public static class MainPage : ContentPage
+    {
+        int count = 0;
+        Button[] = buttons;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            buttons = [CounterBtn1, CounterBtn2, CounterBtn3, CounterBtn4];
+            foreach (Button button in buttons)
+            {
+                button.IsEnabled = false;
+                button.FontSize = 30;
+                button.FontAttributes = FontAttributes.Bold;
+            }
+            Random random = new Random();
+            int index = random.Next(buttons.Length);
+            buttons[index].IsEnabled = true;
+
+        }
+    	
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CountLabel.Text = $"Clicked {count} time";
+            else
+                CountLabel.Text = $"Clicked {count} times";
+        }
+        }
+    }
+}
